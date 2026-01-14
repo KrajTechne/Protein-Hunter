f1410e5b56d474e52a67d8e30e59d6f6ddd8f894
Karthik.Raj@bio-techne.com <Karthik.Raj@bio-techne.com>
2026-01-14 18:34:58 +0000

    Simplified logic for identifying fixed, variable, designable residues.
    Also, allow user to specify whether Vernier Zone residues (identified by CDR +- CDR Extender) should be mutated or not

--- boltz_ph/scfv_utils/ScfvTemplateConstructor.py
+++ boltz_ph/scfv_utils/ScfvTemplateConstructor.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import biotite
 import gemmi
+import json
 import biotite.sequence as seq
 import biotite.structure as struc
 import biotite.structure.io.pdb as pdb
@@ -45,7 +46,7 @@ class ScfvTemplateConstructor:
             struc_atom_array = pdb.get_structure(struc_file, model = 1) # Get first model (Assumption: only one model present)
         elif self.fv_pdb_path.endswith('.cif'):
             struc_file = pdbx.CIFFile.read(self.fv_pdb_path)
-            struc_atom_array = pdbx.get_structure(struc_file, model = 1) #
+            struc_atom_array = pdbx.get_structure(struc_file, model = 1) # Get first model (Assumption: only one model present)
     
         # Extract sequences for each chain from structure atom array
         chain_ids = np.unique(struc_atom_array.chain_id)
@@ -70,8 +71,24 @@ class ScfvTemplateConstructor:
         annotated_paired_seq  = self.scfv_annotator.annotate_seqs(linker_dict, orientation_dict, target_dict={}, generate_motif_commands= False)
         return annotated_paired_seq
     
-    def create_fixed_designable_variable_indices(self, annotated_paired_seq: dict, linker_length: int =20, cdr_extend: int = 2):
-        """ Creates fixed, designable, and variable residues indices based on annotated paired sequence """
+    def create_fixed_designable_variable_array(self, annotated_paired_seq: dict, linker_length: int =20, cdr_extend: int = 2, risk = True):
+        """ Create a list of the linker length with fixed, variable, designable residues
+            class_mapping = {'variable': 0, 'fixed': 1, 'designable': 2}
+            Approach:
+                1. Define list of 0s of desired scfv length
+                2. Fill in at specified positions (designable or fixed) with the appropriate value: 1 or 2
+                3. Remaining values are by default variable residues and already have value of 0
+                4. Return np array of class_mapping values
+            Args:
+                annotated_paired_seq (dict): Dictionary of annotated paired sequence
+                linker_length (int): Length of linker
+                cdr_extend (int): Number of residues to extend CDR Definition by on both sides of CDR region
+                risk (bool): Allows for mutation of potential Vernier Zone Residues
+            Returns:
+                scfv_mapping (np.ndarray): Numpy array of class_mapping values
+                paratope_residues_str (str): String of 1-indexed residue positions in paratope separated by commas
+                cys_indices_one_indexed (list): List of 1-indexed residue positions of cysteine residues in antibody framework regions 
+        """
 
         if self.orientation == "VH-VL":
             first_chain = "heavy"
@@ -86,19 +103,25 @@ class ScfvTemplateConstructor:
         sec_chain_length = len(target_annotation[sec_chain]['seq'])
         print(f"Heavy Chain Regions: {target_annotation['heavy']['region_seqs_dict']}")
         print(f"Light Chain Regions: {target_annotation['light']['region_seqs_dict']}")
-        #print(f"Seq Lengths: First Chain {first_chain_length}, Second Chain {sec_chain_length}, Linker Length: {linker_length}")
-        #print(f"Total Length: {first_chain_length + sec_chain_length + linker_length}")
 
-        indices_dict = {'fixed': [], 'designable': [], 'variable': []}
-        paratope_indices = []
-
+        # ScFv Mapping list
+        scfv_length = first_chain_length + sec_chain_length + linker_length
+        self.residue_mapping_dict = {'variable': 0, 'fixed': 1, 'designable': 2} # Mapping of residue types to integers for use in template construction
+         # Save as class variable to use in next step
+        scfv_mapping = np.zeros(scfv_length, dtype=int)
+        paratope_residues_zero_indexed = np.zeros(scfv_length, dtype=int)
+        
+        # For Linker region
+        scfv_mapping[first_chain_length : first_chain_length + linker_length] = self.residue_mapping_dict['designable']
+        
+        cys_indices_full = []
         for chain, chain_dict in target_annotation_chains.items():
     
             # Determine residue offset based on chain order
             if chain == first_chain:
-                residue_offset = 1 # Residue numbering starts at 1
+                residue_offset = 0  
             else:
-                residue_offset = first_chain_length + linker_length + 1 # +1 to account for 1-based indexing. Account for linker length and first chain length
+                residue_offset = first_chain_length + linker_length  # Account for linker length and first chain length
     
             # For each region in the chain
             for region, region_index_dict in chain_dict['region_loc_dict'].items():
@@ -112,54 +135,63 @@ class ScfvTemplateConstructor:
                 adjusted_indices_list = [i + residue_offset for i in zero_indices_list]
 
                 # Assign to appropriate category in indices_dict
-                if "fmwk" in region:    
-                    indices_dict['variable'].extend(adjusted_indices_list)
+                if "fmwk" in region:
+                    # Check whether Cysteine is in fmwk region -> must mutate  out -> index should be part of designable residues
+                    seq_fmwk = chain_dict['region_seqs_dict'][region]
+                    # 1. Find all indices where 'C' appears
+                    cys_indices_list = [adjusted_indices_list[i] for i, char in enumerate(seq_fmwk) if char == "C"]
+                    cys_indices_full.extend(cys_indices_list)
+                    # 2. If any are found, move them to designable and remove from original list
+                    if cys_indices_list:
+                        # 2.2 Assign them as class: designable 
+                        for cys_index in cys_indices_list:
+                            scfv_mapping[cys_index] = self.residue_mapping_dict['designable']
+                    
                 elif "cdr" in region:
-                    indices_dict['fixed'].extend(adjusted_indices_list)
+                    # Extend CDR definition by cdr_extend on both sides (Accounts for discrepancies between antibody annotation schemes on CDR definitions)
+                    extended_start = max(start - cdr_extend + residue_offset, 0) # Ensure start does not go below 0
+                    extended_end = min(end + cdr_extend + residue_offset + 1, scfv_length) # Ensure end does not exceed scfv length. + 1 to include end index, since end is exclusive 
+                    # Key Change: Define paratope based on the extended start and end indices. Restrict CDR definition to original Martin definition
+                    # Advantage: Allow model to design/operate on Vernier Zone residues
+                    # Risk: Model could mutate potential Vernier Zone residues to potentially intolerable residues
+                    paratope_residues_zero_indexed[extended_start: extended_end] = 1
+                    if risk: # Allowing model to mutate potential Vernier Zone residues                 
+                        scfv_mapping[start: end + 1] = self.residue_mapping_dict['fixed'] # +1 to include end index
+                    else: # Do not allow model to mutate potential Vernier Zone residues
+                        scfv_mapping[extended_start: extended_end] = self.residue_mapping_dict['fixed']
 
-                    # Extract residues potentially responsible for binding (extend CDRs by cdr_extend on both sides, within chain limits)
-                    # Aim: Capture potential paratope residues and pass to Protein Hunter to ensure contact check only occurs with these residues and target epitope
-                    extended_start = max(start - cdr_extend, 0) # Ensure start does not go below 0
-                    extended_end = min(end + cdr_extend, len(chain_dict['seq']) - 1) # Ensure end does not exceed chain length - 1
-                    extended_zero_indices_list = list(range(extended_start, extended_end + 1))
-                    extended_adjusted_indices_list = [i + residue_offset for i in extended_zero_indices_list]
-                    paratope_indices.extend(extended_adjusted_indices_list)
-        
-        # For Linker region
-        indices_dict['designable'] = list(range(first_chain_length + 1, first_chain_length + linker_length + 1))
+        # Convert Paratope Residues Indices to a 1-indexed string format for Protein Hunter input (separated by commas)
+        paratope_indices_one_indexed = [index + 1 for index, val in enumerate(paratope_residues_zero_indexed) if val == 1]
+        #print("Paratope Indices 1-indexed: ", paratope_indices_one_indexed)
+        paratope_indices_str = ','.join([str(i) for i in paratope_indices_one_indexed])
+        #print("Paratope Indices String: ", paratope_indices_str)
+        # Convert Cys indices to 1-indexed list
+        cys_indices_one_indexed = [val + 1 for val in cys_indices_full]
 
-        # Convert Paratope Residues Indices to a string format for Protein Hunter input (separated by commas)
-        paratope_indices_str = ','.join([str(i) for i in paratope_indices])
-
-        #print("Fixed indices:", indices_dict['fixed'])
-        #print("Designable indices:", indices_dict['designable'])
-        #print("Variable indices:", indices_dict['variable'])
-
-        #print(f"Number of Fixed Residues: {len(indices_dict['fixed'])}")
-        #print(f"Number of Designable Residues: {len(indices_dict['designable'])}")
-        #print(f"Number of Variable Residues: {len(indices_dict['variable'])}")
-        return indices_dict, paratope_indices_str
+        return scfv_mapping, paratope_indices_str, cys_indices_one_indexed
     
-    def create_final_fixed_designable_dict(self, fixed_designable_variable_indices: dict, probs_dict: dict):
+    def create_final_fixed_designable_dict(self, scfv_mapping: np.ndarray, probs_dict: dict):
         """ Creates final fixed and designable dict for Protein Hunter input based on fixed, designable, and variable indices """
         
         # Establish sequence length
-        seq_len = max(fixed_designable_variable_indices['fixed'] + fixed_designable_variable_indices['designable'] + fixed_designable_variable_indices['variable'])
-        #print(f"Length of seq: {len(fixed_designable_variable_indices['fixed'] + fixed_designable_variable_indices['designable'] + fixed_designable_variable_indices['variable'])}")
-        #print("Sequence Length:", seq_len)
-        
+        seq_len = len(scfv_mapping)
+
         # Create initial dict to store residue categories: fixed and designable 
         final_fixed_designable_dict = {'fixed': [], 'designable': []}
         res_designable_dict = {}
-        for res_1pos in range(1, seq_len + 1):
-
-            # Determine category of residue
-            if res_1pos in fixed_designable_variable_indices['fixed']:
+        for index in range(seq_len):
+            # Extract class_int at index position
+            category_int = scfv_mapping[index]
+            # Determine category of residue based on category int
+            if category_int == self.residue_mapping_dict['fixed']:
                 category = 'fixed'
-            elif res_1pos in fixed_designable_variable_indices['designable']:
+            elif category_int == self.residue_mapping_dict['designable']:
                 category = 'designable'
             else:
                 category = 'variable'
+            
+            # Convert index position to residue position (1-indexed)
+            res_1pos = index + 1
             
             # Assign residue to final dict based on category and associated probability
             prob = probs_dict[category]
@@ -274,16 +306,51 @@ class ScfvTemplateConstructor:
         print(f"✅ Converted to CIF: {output_cif_path}")
         return output_cif_path
     
-    def create_protein_hunter_inputs(self, output_file_path, linker_length=20, cdr_extend: int = 3, probs_dict: dict = {'fixed': 0.0, 'variable': 0.7, 'designable': 1.01}):
+    def create_mpnn_per_residue_bias(self, cys_residue_numbers: list, output_json_path: str = "/tmp/mpnn_per_residue_bias.json"):
+        """ Creates the JSON file for the per-residue bias for the cysteines involved in the disulfide bond in original Antibody Framework Regions
+            Cysteines typically located in positions: 23 and 87.
+            Per-residue bias will ensure cysteines get mutated to common hydrophobic residues typically used for this specific scenario
+            Goal: Create JSON file and save to /tmp/mpnn_per_residue_bias.json
+            Args:
+                cys_residue_numbers (list): List of residue numbers for cysteines involved in disulfide bond
+                output_json_path (str): Path to save the JSON file
+            Return:
+                output_json_path (str): Path to the saved JSON file
+        """
+        # 1. Setup your staples (High positive bias = forced selection)
+        staple_residues = ["V", "I", "L", "A", "M"]
+        staple_bias = 100.0  # Strong Positive bias imposes MPNN to select between residues
+
+        # 2. Create the dictionary
+        # Format: {"H23": {"V": 100.0, "I": 100.0, ...}, "H88": {...}}
+        bias_dict = {} 
+        for pdb_idx in cys_residue_numbers:
+            # Construct key: pdb chain + residue number, e.g., "H23", "A88", etc. (typical PDB notation)
+            key = f"A{pdb_idx}"
+            
+            # Initialize position
+            bias_dict[key] = {}
+            # Add bias for each hydrophobic staple
+            for aa in staple_residues:
+                bias_dict[key][aa] = staple_bias
+
+        # 3. Save to a temporary JSON file
+        with open(output_json_path, "w") as f:
+            json.dump(bias_dict, f, indent=2)
+        print(f"Bias file created at {output_json_path} for indices: {list(bias_dict.keys())}")
+        return output_json_path
+    
+    def create_protein_hunter_inputs(self, output_file_path, linker_length=20, cdr_extend: int = 2, 
+                                     probs_dict: dict = {'fixed': 0.0, 'variable': 0.7, 'designable': 1.01}):
         """ Creates the single chain PDB file and extract seq input for Protein Hunter inputs """
         
         # 1. Extract chain sequences
         seq_dict, paired_seq = self.extract_chain_sequences()
         # 2. Annotate paired sequence
         annotated_paired_seq = self.annotate_paired_sequence(paired_seq)
-        # 3. Create fixed/designable indices
-        fixed_designable_variable_indices, paratope_indices = self.create_fixed_designable_variable_indices(annotated_paired_seq, linker_length=linker_length, 
-                                                                                                            cdr_extend=cdr_extend)
+        # 3. Create mapping of fixed/designable/variable indices
+        fixed_designable_variable_indices, paratope_indices, cys_indices = self.create_fixed_designable_variable_array(annotated_paired_seq,
+                                                                                                                         linker_length=linker_length, cdr_extend=cdr_extend)
         # 4. Create Final Fixed and Designable Dict 
         final_fixed_designable_dict, sc_res_designable_dict = self.create_final_fixed_designable_dict(fixed_designable_variable_indices, probs_dict=probs_dict)
         # Create single chain PDB file with linker
@@ -292,7 +359,10 @@ class ScfvTemplateConstructor:
         # 5. Convert to mmCIF format
         output_cif_path = self.convert_pdb_to_cif(input_pdb_path=output_file_path)
 
+        # 6. Create JSON file for per-residue bias. Need to mutate cysteine residues to common hydrophobic residues
+        json_path = self.create_mpnn_per_residue_bias(cys_residue_numbers=cys_indices)
+
         print("Number of Fixed Residues:", len(final_fixed_designable_dict['fixed']))
         print("Number of Designable Residues:", len(final_fixed_designable_dict['designable']))
         print("✅ Created Protein Hunter Inputs")
-        return seq_dict, paired_seq, seq_input, output_cif_path, final_fixed_designable_dict, paratope_indices
+        return seq_dict, paired_seq, seq_input, output_cif_path, final_fixed_designable_dict, paratope_indices, json_path
